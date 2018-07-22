13ea1a5072dce468982eb3d55bd7e8435000f29b
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-22 16:54:19 -0500

    started a QuoteControllerTest file

---
+++ src/test/java/com/microservice/quotes/controllers/QuoteControllerTest.java
@@ -0,0 +1,16 @@
+package com.microservice.quotes.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuoteControllerTest {
+
+    @Test
+    public void findSymbolById() {
+    }
+
+    @Test
+    public void findSymbolByIdAndDate() {
+    }
+}
