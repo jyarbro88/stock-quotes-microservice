912e80791198da4f21d25a9166bcab5c98620b12
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-23 11:15:59 -0500

    refactored RestTemplate bean and placed in in the main application page

--- src/main/java/com/microservice/quotes/QuotesApplication.java
+++ src/main/java/com/microservice/quotes/QuotesApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -21,5 +23,11 @@ public class QuotesApplication {
 
         JsonReader jsonReader = new JsonReader();
         return args -> jsonReader.readJsonFile(quoteRepository);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
--- src/main/java/com/microservice/quotes/controllers/QuoteController.java
+++ src/main/java/com/microservice/quotes/controllers/QuoteController.java
@@ -7,8 +7,6 @@ import com.microservice.quotes.repositories.QuoteRepository;
 import com.microservice.quotes.utilities.MicroServiceConnector;
 import com.microservice.quotes.utilities.StockModelBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -59,11 +57,5 @@ public class QuoteController {
         dailyStockModel.setTickerSymbol(String.valueOf(companySymbol));
         dailyStockModel.setCompanyName(String.valueOf(companyName));
         return dailyStockModel.toString();
-    }
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
     }
 }
